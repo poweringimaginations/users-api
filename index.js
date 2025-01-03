51ab8d767a7f5f4cebd8dc71fa24bacf9b7fa4f8
poweringimaginations <bradychiu2028@u.northwestern.edu>
2025-01-03 21:56:27 +0800

    Add files via upload
    whoops

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+const { Pool } = require('pg'); 
+
+const app = express();
+const PORT = 3000;
+
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+pool.connect()
+    .then(() => console.log('Connected to the database'))
+    .catch((err) => console.error('Database connection error', err));
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+app.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
